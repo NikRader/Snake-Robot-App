45068b48e2ac6649719e579b0c014360e282420f
Kudryavtsev Nikita <113279023+NikRader@users.noreply.github.com>
2024-05-14 22:23:50 +0300

    add Robot_Controll_page_activity

--- BluetoothModuleDef/Bluetooth_def-main/BluetoothModuleDef/app/src/main/java/com/example/bluetoothmoduledef/MainFragment.kt
+++ BluetoothModuleDef/Bluetooth_def-main/BluetoothModuleDef/app/src/main/java/com/example/bluetoothmoduledef/MainFragment.kt
@@ -3,6 +3,7 @@ package com.example.bluetoothmoduledef
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,7 +17,7 @@ import com.example.bt_def.bluetooth.BluetoothController
 
 class MainFragment : Fragment(), BluetoothController.Listener {
 
-   private lateinit var binding: FragmentMainBinding
+    private lateinit var binding: FragmentMainBinding
     private lateinit var bluetoothController: BluetoothController
     private lateinit var btAdapter: BluetoothAdapter
     override fun onCreateView(
@@ -32,15 +33,19 @@ class MainFragment : Fragment(), BluetoothController.Listener {
 
         initBtAdapter()
         val pref = activity?.getSharedPreferences(
-        BluetoothConstans.PREFERENCES, Context.MODE_PRIVATE)
+            BluetoothConstans.PREFERENCES, Context.MODE_PRIVATE
+        )
         val mac = pref?.getString(BluetoothConstans.MAC, "")
         bluetoothController = BluetoothController(btAdapter)
 
         binding.bList.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_deviceListFragment)
         }
-        binding.connectBt.setOnClickListener(){
-            bluetoothController.connect(mac?: "", this)
+        binding.toRobotControllBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_Robot_controller)
+        }
+        binding.connectBt.setOnClickListener() {
+            bluetoothController.connect(mac ?: "", this)
         }
         binding.TurnOnBtn.setOnClickListener {
             bluetoothController.sendMessage("A")
@@ -49,28 +54,31 @@ class MainFragment : Fragment(), BluetoothController.Listener {
             bluetoothController.sendMessage("B")
         }
 
+
     }
 
     private fun initBtAdapter() {
         val bManager = activity?.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         btAdapter = bManager.adapter
     }
-        // Метод для оповещения о подлючении устройства
+
+    // Метод для оповещения о подлючении устройства
     override fun onReceive(message: String) {
         activity?.runOnUiThread {
-            when(message){
-               BluetoothController.BLUETOOTH_CONNECTED ->{
+            when (message) {
+                BluetoothController.BLUETOOTH_CONNECTED -> {
                     binding.connectBt.backgroundTintList = AppCompatResources
                         .getColorStateList(requireContext(), com.example.bt_def.R.color.red)
-                            binding.connectBt.text ="Отключиться"
-               }
-                BluetoothController.BLUETOOTH_NO_CONNECTED ->{
+                    binding.connectBt.text = "Отключиться"
+                }
+
+                BluetoothController.BLUETOOTH_NO_CONNECTED -> {
                     binding.connectBt.backgroundTintList = AppCompatResources
                         .getColorStateList(requireContext(), com.example.bt_def.R.color.green)
-                    binding.connectBt.text ="Подключиться"
-
+                    binding.connectBt.text = "Подключиться"
                 }
-                else ->{
+
+                else -> {
 
                     binding.tvStatus.text = message
                 }
